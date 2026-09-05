54300f7c1c279a63241fb24cb7a391243cf2e513
meowcat <meowcat@meowcats.space>
2026-09-05 22:31:09 +0100

    create base readylistener

---
+++ bot/src/main/java/cult/konata/nanako/listeners/ReadyListener.java
@@ -0,0 +1,30 @@
+package cult.konata.nanako.listeners;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+
+import java.util.EventListener;
+
+public class ReadyListener implements EventListener {
+        public void setupBot(String token) {
+            JDA jda = JDABuilder.createDefault(token)
+                    .addEventListeners(new ReadyListener())
+                    .build();
+
+            // block until JDA is ready
+            try {
+                jda.awaitReady();
+            } catch (Exception e) {
+                System.out.println("Could not awaitReady! Printing stack trace!");
+                e.printStackTrace();
+            }
+        }
+
+    public void onEvent(GenericEvent event) {
+            if (event instanceof ReadyEvent) {
+                System.out.println("API is ready!");
+            }
+        }
+}
