be0864e60d7e7023cb315dddb5c24280638e7e99
meowcat <meowcat@meowcats.space>
2026-09-05 22:16:30 +0100

    initial commit

---
+++ bot/src/main/java/cult/konata/nanako/Main.java
@@ -0,0 +1,13 @@
+package cult.konata.nanako;
+
+public class Main {
+    public static void main(String[] args) {
+        // We expect the passed parameter to be the bot token, so its 0.
+        if (args[0] == "") {
+            System.out.println("No bot token passed. Stopping.");
+            System.out.println("usage: nanako [token]");
+        } else {
+
+        }
+    }
+}
