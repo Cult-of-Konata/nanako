da2efe04cc157d49a684882f19d9c12507b3f099
meowcat <meowcat@meowcats.space>
2026-09-06 10:15:41 +0100

    set up doxygen

--- bot/src/main/java/cult/konata/nanako/Main.java
+++ bot/src/main/java/cult/konata/nanako/Main.java
@@ -2,12 +2,9 @@ package cult.konata.nanako;
 
 public class Main {
     public static void main(String[] args) {
-        // We expect the passed parameter to be the bot token, so its 0.
-        if (args[0] == "") {
-            System.out.println("No bot token passed. Stopping.");
-            System.out.println("usage: nanako [token]");
-        } else {
-
-        }
+        /**
+         * Pass the token as "-Dtoken=...".
+         */
+        String token = System.getProperty("token");
     }
 }
